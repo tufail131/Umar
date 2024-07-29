23144af0e83337e8611ff04e8443cb06b3470c5f
tufail131 <tr611969@gmail.com>
2024-07-30 00:59:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7269921365:AAHAmCkhA9g-LiZzpVjt_fL0LZzWSK79-AY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("koina1997" "TufailRaza94")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("715186742" "1453106730")
+
+
+
+
+
+
+
+  
